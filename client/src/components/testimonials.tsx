78fc032cf66d87bc19cea106a98b9802ece49fcc
markoneo <34494358+markoneo@users.noreply.github.com>
2025-07-29 22:42:14 +0200

    Fix extra closing div in testimonials component

--- client/src/components/testimonials.tsx
+++ client/src/components/testimonials.tsx
@@ -79,7 +79,6 @@ export default function Testimonials() {
                 {[...Array(5)].map((_, i) => (
                   <Star key={i} className="w-5 h-5 text-gold fill-current" />
                 ))}
-                </div>
               </div>
               <p className="text-slate mb-6 italic">"{testimonial.text}"</p>
               <div className="flex items-center">
