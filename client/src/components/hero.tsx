81d4595a85e9786a967ab856eade9f451d6eb8ea
markoneo <34494358+markoneo@users.noreply.github.com>
2025-07-29 22:53:59 +0200

    Fix incomplete img tag in hero component

--- client/src/components/hero.tsx
+++ client/src/components/hero.tsx
@@ -73,7 +73,6 @@ export default function Hero() {
               className="rounded-3xl shadow-lg w-full object-cover"
               data-testid="img-hero-consultation"
             />
-          </div>
         </div>
       </div>
     </section>
